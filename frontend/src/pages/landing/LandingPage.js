994e3f09b592fe17472da227d172c847c8dca2e1
zsoe <zwemsoe@gmail.com>
2022-01-15 19:21:41 -0800

    done setup profile

--- frontend/src/pages/landing/LandingPage.js
+++ frontend/src/pages/landing/LandingPage.js
@@ -1,23 +1,19 @@
 import * as React from "react";
-import { Button, Box, HStack, Center, Container } from "@chakra-ui/react";
+import { Box, Flex } from "@chakra-ui/react";
 import { SetupProfile } from "./SetupProfile";
 
 export const LandingPage = () => {
   return (
-    <Container>
-      <Center>
-        <HStack>
-          <Box minW='xl' w='100%' bg='tomato' p='4'>
-            1
-          </Box>
-          <Box minW='xl' w='100%' minV='100%' p='4'>
-            <Center>
+    <Flex justify='space-between'>
+      <Box minW='xl' w='100%' minH='100vh' bg='tomato' p='4'>
+        Description
+      </Box>
+      <Box minW='xl' w='100%' minH='100%' p='4'>
+        {/* <Center>
               <Button>Sign in with Google</Button>
-            </Center>
-            {/* <SetupProfile /> */}
-          </Box>
-        </HStack>
-      </Center>
-    </Container>
+            </Center> */}
+        <SetupProfile />
+      </Box>
+    </Flex>
   );
 };
