61602593622ff6fcb203002201bafe7eab014717
zsoe <zwemsoe@gmail.com>
2022-01-15 20:16:06 -0800

    auth test

--- frontend/src/firebase.js
+++ frontend/src/firebase.js
@@ -4,27 +4,30 @@ import { getAnalytics } from "firebase/analytics";
 import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
 
 const provider = new GoogleAuthProvider();
-const login = async() => {
-    const auth = getAuth();
-    signInWithPopup(auth, provider)
-        .then((result) => {
-            // This gives you a Google Access Token. You can use it to access the Google API.
-            const credential = GoogleAuthProvider.credentialFromResult(result);
-            const token = credential.accessToken;
-            // The signed-in user info.
-            const user = result.user;
-            // ...
-        }).catch((error) => {
-            // Handle Errors here.
-            const errorCode = error.code;
-            const errorMessage = error.message;
-            // The email of the user's account used.
-            const email = error.email;
-            // The AuthCredential type that was used.
-            const credential = GoogleAuthProvider.credentialFromError(error);
-            // ...
-        });
-}
+const login = async () => {
+  const auth = getAuth();
+  signInWithPopup(auth, provider)
+    .then((result) => {
+      // This gives you a Google Access Token. You can use it to access the Google API.
+      const credential = GoogleAuthProvider.credentialFromResult(result);
+      const token = credential.accessToken;
+      // The signed-in user info.
+      const user = result.user;
+
+      console.log(user);
+      // ...
+    })
+    .catch((error) => {
+      // Handle Errors here.
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      // The email of the user's account used.
+      const email = error.email;
+      // The AuthCredential type that was used.
+      const credential = GoogleAuthProvider.credentialFromError(error);
+      // ...
+    });
+};
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -32,13 +35,13 @@ const login = async() => {
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyBrm8MJo8UHxYaso8tXSWDJ4Dhy3ohVQmg",
-    authDomain: "nwhacks-f42df.firebaseapp.com",
-    projectId: "nwhacks-f42df",
-    storageBucket: "nwhacks-f42df.appspot.com",
-    messagingSenderId: "717144736521",
-    appId: "1:717144736521:web:c523e58d3cda7c84d949d8",
-    measurementId: "G-N67G6JCZ9T"
+  apiKey: "AIzaSyBrm8MJo8UHxYaso8tXSWDJ4Dhy3ohVQmg",
+  authDomain: "nwhacks-f42df.firebaseapp.com",
+  projectId: "nwhacks-f42df",
+  storageBucket: "nwhacks-f42df.appspot.com",
+  messagingSenderId: "717144736521",
+  appId: "1:717144736521:web:c523e58d3cda7c84d949d8",
+  measurementId: "G-N67G6JCZ9T",
 };
 
 // Initialize Firebase
--- frontend/src/pages/landing/LandingPage.js
+++ frontend/src/pages/landing/LandingPage.js
@@ -1,23 +1,25 @@
-import * as React from "react";
-import { Box, Flex } from "@chakra-ui/react";
+import { useEffect } from "react";
+import { Box, Flex, Center, Button } from "@chakra-ui/react";
 import { SetupProfile } from "./SetupProfile";
 import { login } from "../../firebase.js";
+import { getAuth } from "firebase/auth";
 
 export const LandingPage = () => {
+  const auth = getAuth();
+  useEffect(() => {
+    console.log(auth);
+  }, [auth]);
+
   return (
     <Flex justify='space-between'>
       <Box minW='xl' w='100%' minH='100vh' bg='tomato' p='4'>
         Description
       </Box>
       <Box minW='xl' w='100%' minH='100%' p='4'>
-        {/* <Center>
-<<<<<<< HEAD
-              <Button>Sign in with Google</Button>
-=======
-              <Button onClick={login}>Sign in with Google</Button>
->>>>>>> auth
-            </Center> */}
-        <SetupProfile />
+        <Center>
+          <Button onClick={login}>Sign in with Google</Button>
+        </Center>
+        {/* <SetupProfile /> */}
       </Box>
     </Flex>
   );
